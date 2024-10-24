0b731403515283dfaacfcbaa700e4d0b746f2bc0
Ryan Turaids <ryturaids@gmail.com>
2024-10-23 19:00:59 -0700

    added secure: false to transporter

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.post("/send-email", async (req, res) => {
     let transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
